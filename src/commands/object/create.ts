2af7550c43e43ad459b11b98dadf1858323c7e06
Chandan Rai <chandan@nimbella.com>
2020-08-19 22:25:57 +0530

    [object:create] add destination option

--- src/commands/object/create.ts
+++ src/commands/object/create.ts
@@ -13,9 +13,9 @@
 
 import { Bucket } from '@google-cloud/storage'
 import { flags } from '@oclif/command'
+import { basename, isAbsolute, join } from 'path'
+import { existsSync, lstatSync } from 'fs'
 import { spinner } from '../../ui'
-import { basename } from 'path';
-import { existsSync } from 'fs';
 import { NimBaseCommand, NimLogger } from 'nimbella-deployer'
 import { authPersister } from 'nimbella-deployer'
 import { getObjectStorageClient } from '../../storage/clients'
@@ -25,6 +25,7 @@ export default class ObjectCreate extends NimBaseCommand {
 
     static flags = {
         apihost: flags.string({ description: 'API host of the namespace to add object to' }),
+        destination: flags.string({ char: 'd', description: 'Target location in object storage' }),
         ...NimBaseCommand.flags
     }
 
@@ -33,24 +34,46 @@ export default class ObjectCreate extends NimBaseCommand {
         { name: 'namespace', description: 'The namespace to add object to (current namespace if omitted)', required: false }
     ]
 
-    static aliases = ['objects:add', 'object:add'];
+    static aliases = ['objects:add', 'object:add']
 
     async runCommand(rawArgv: string[], argv: string[], args: any, flags: any, logger: NimLogger) {
-        const { client } = await getObjectStorageClient(args, flags, authPersister);
-        if (!client) logger.handleError(`Couldn't get to the object store, ensure it's enabled for the ${args.namespace || 'current'} namespace`);
-        await this.uploadFile(args.objectPath, client, logger).catch((err: Error) => logger.handleError('', err));
+        const { client } = await getObjectStorageClient(args, flags, authPersister)
+        if (!client) logger.handleError(`Couldn't get to the object store, ensure it's enabled for the ${args.namespace || 'current'} namespace`)
+        await this.uploadFile(args.objectPath, flags.destination, client, logger).catch((err: Error) => logger.handleError('', err))
     }
 
-    async uploadFile(objectPath: string, client: Bucket, logger: NimLogger) {
-        if (!existsSync(objectPath)){
+    async uploadFile(objectPath: string, destination: string, client: Bucket, logger: NimLogger) {
+        if (!existsSync(objectPath)) {
             logger.log(`${objectPath} doesn't exist`)
             return
         }
-        const loader = await spinner();
-        const objectName = basename(objectPath);
-        loader.start(`adding ${objectName}`, 'uploading', {stdout: true})
+        if (!lstatSync(objectPath).isFile()) {
+            logger.log(`${objectPath} is not a valid file`)
+            return
+        }
+        const objectName = basename(objectPath)
+
+        let targetPath = objectPath
+        if (isAbsolute(objectPath)) targetPath = objectName
+        if (destination) {
+            if (destination.endsWith('/'))
+                targetPath = join(destination, objectName)
+            else
+                targetPath = destination
+        }
+
+        const loader = await spinner()
+
+        const [exists] = await client.file(targetPath).exists()
+        if (exists) {
+            logger.log(`${targetPath} already exists, use 'object:update' to update it. e.g. nim object update ${objectName}`)
+            return
+        }
+
+        loader.start(`adding ${objectName}`, 'uploading', { stdout: true })
         await client.upload(objectPath, {
-            gzip: true
-        }).then(_ => loader.stop('done'));
+            destination: targetPath,
+            gzip: true,
+        }).then(_ => loader.stop('done'))
     }
 }
