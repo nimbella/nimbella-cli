f7993b4c339bd2e92e1d3e04d629e6a0232d0ae6
Joshua Auerbach <joshuaauerbachwatson@gmail.com>
2020-12-29 07:38:49 -0500

    Complete functionality for awsprovider
    Test needs updating to cover the added functionality.

--- awsprovider/src/index.ts
+++ awsprovider/src/index.ts
@@ -11,21 +11,24 @@
  * governing permissions and limitations under the License.
  */
 
-import { StorageProvider, StorageClient, RemoteFile, DeleteFilesOptions, DownloadOptions, GetFilesOptions,
-	SaveOptions, SignedUrlOptions, UploadOptions, StorageKey } from '@nimbella/storage-provider'
-import { S3, GetObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3'
+import { StorageProvider, StorageClient, RemoteFile, DeleteFilesOptions, DownloadOptions, GetFilesOptions, SaveOptions, 
+	SignedUrlOptions, UploadOptions, StorageKey, SettableFileMetadata, WebsiteOptions, FileMetadata } 
+	from '@nimbella/storage-provider'
+import { S3Client, GetObjectCommand, PutObjectCommand, ListObjectsCommand, DeleteObjectCommand, DeleteObjectsCommand, 
+	PutBucketWebsiteCommand, CopyObjectCommand, HeadObjectCommand } from '@aws-sdk/client-s3'
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 import { Readable, Writable } from 'stream'
-import { createWriteStream } from 'fs'
+import { createWriteStream, createReadStream } from 'fs'
 import { WritableStream } from 'memory-streams'
 import * as makeDebug from 'debug'
 const debug = makeDebug('nim:storage-s3')
  
 class S3RemoteFile implements RemoteFile {
-	private s3: S3
+	private s3: S3Client
 	private bucketName: string
 	name: string
 
-    constructor(s3: S3, bucketName: string, fileName: string) {
+    constructor(s3: S3Client, bucketName: string, fileName: string) {
 		debug('created file handle for %s', fileName)
 		this.s3 = s3
 		this.bucketName = bucketName
@@ -37,24 +40,32 @@ class S3RemoteFile implements RemoteFile {
 	}
 
 	save(data: Buffer, options?: SaveOptions): Promise<any> {
-		const cmd = new PutObjectCommand({Bucket: this.bucketName, Key: this.name, Body: data, Metadata: options?.metadata })
+		const { contentType: ContentType, cacheControl: CacheControl } = options?.metadata
+		const cmd = new PutObjectCommand({ Bucket: this.bucketName, Key: this.name, Body: data, ContentType, CacheControl })
 		return this.s3.send(cmd)
 	}
 
-	setMetadata(meta: Record<string, any>): Promise<any> {
-		return undefined
+	setMetadata(meta: SettableFileMetadata): Promise<any> {
+		const CopySource = `${this.bucketName}/${this.name}`
+		const { cacheControl: CacheControl, contentType: ContentType } = meta
+		const cmd = new CopyObjectCommand({ CopySource, Bucket: this.bucketName, Key: this.name, CacheControl, ContentType })
+		return this.s3.send(cmd)
 	}
 
-    async getMetadata(): Promise<Record<string, any>> {
-    	return undefined
+    async getMetadata(): Promise<FileMetadata|undefined> {
+		const cmd = new HeadObjectCommand({ Bucket: this.bucketName, Key: this.name})
+		const response = await this.s3.send(cmd)
+		const { StorageClass: storageClass, ContentLength, ETag: etag, LastModified } = response
+		return { name: this.name, storageClass, size: String(ContentLength), etag, updated: LastModified.toISOString() } 
 	}
 
 	async exists(): Promise<boolean> {
-		return undefined
+		return !!await this.getMetadata()
 	}
 	
     delete(): Promise<any> {
-		return undefined
+		const cmd = new DeleteObjectCommand({Bucket: this.bucketName, Key: this.name })
+		return this.s3.send(cmd)
 	}
 	
 	async download(options?: DownloadOptions): Promise<Buffer> {
@@ -65,15 +76,40 @@ class S3RemoteFile implements RemoteFile {
 		debug('will send command: %O', cmd)
 		const result = await this.s3.send(cmd)
 		debug('got back result: %O', result)
-		const content = result.Body as Readable // TODO what if Body isn't a Readable?
+		const content = result.Body as Readable // Body has type ReadableStream<any>|Readable|Blob.  Readable seems to work in practice
 		debug('about to pipe')
 		await pipe(content, destination)
 		debug('piping complete')
 		return options?.destination ? Buffer.from('') : (destination as WritableStream).toBuffer()
 	}
 
-	async getSignedUrl(options?: SignedUrlOptions): Promise<string> {
-		return undefined
+	async getSignedUrl(options: SignedUrlOptions): Promise<string> {
+		const { action, expires, version, contentType: ContentType } = options
+		if (version !== 'v4') {
+			throw new Error('Signing version v4 is required for s3')
+		}
+		let expiresIn: number
+		if (expires) {
+			// The interface expects something that can be passed to new Date()
+			// to get an absolute time.  The s3 getSignedUrl options expect a value in seconds
+			// from now
+			expiresIn = (new Date(expires).getTime() - Date.now()) / 1000
+		}
+		let cmd
+		switch (action) {
+		case 'read':
+			cmd = new GetObjectCommand({ Bucket: this.bucketName, Key: this.name })
+			break
+		case 'write':
+			cmd = new PutObjectCommand({ Bucket: this.bucketName, Key: this.name, ContentType })
+			break
+		case 'delete':
+			cmd = new DeleteObjectCommand({ Bucket: this.bucketName, Key: this.name })
+		}
+		if (!cmd) {
+			throw new Error(`The action for a signed URL must be one of 'read' | 'write' | 'delete'`)
+		}
+		return getSignedUrl(this.s3, cmd, { expiresIn })
 	}
  }
 
@@ -90,12 +126,12 @@ function pipe(input: Readable, output: Writable): Promise<unknown> {
 	return toWait
  }
 
- class S3Client implements StorageClient {
-	private s3: S3
+ class NimS3Client implements StorageClient {
+	private s3: S3Client
 	private bucketName: string
 	private url: string
 	 
-	constructor(s3: S3, bucketName: string, url: string) {
+	constructor(s3: S3Client, bucketName: string, url: string) {
 		debug('s3client: %O, bucketName=%s, url=%s', s3.config, bucketName, url)
 		this.s3 = s3
 		this.bucketName = bucketName
@@ -110,12 +146,20 @@ function pipe(input: Readable, output: Writable): Promise<unknown> {
 		return this.url
 	}
 	
-	setMetadata(meta: Record<string, any>): Promise<any> {
-		return undefined
+	setWebsite(website: WebsiteOptions): Promise<any> {
+		const { notFoundPage: Key, mainPageSuffix: Suffix } = website
+		const cmd = new PutBucketWebsiteCommand({ Bucket: this.bucketName, WebsiteConfiguration: { ErrorDocument: { Key }, IndexDocument: { Suffix } }})
+		return this.s3.send(cmd)
 	}
 
-	deleteFiles(options?: DeleteFilesOptions): Promise<any> {
-		return undefined
+	async deleteFiles(options?: DeleteFilesOptions): Promise<any> {
+		// s3 does not support 'prefix' on DeleteObjects, only on ListObjects.
+		// The multi-object delete takes a list of objects.  So this takes two round trips.
+		const listCmd = new ListObjectsCommand({ Bucket: this.bucketName, Prefix: options?.prefix })
+		const listResult = await this.s3.send(listCmd)
+		const Objects = listResult.Contents.map(obj => ({ Key: obj.Key }))
+		const deleteCmd = new DeleteObjectsCommand({ Bucket: this.bucketName, Delete: { Objects } })
+		return this.s3.send(deleteCmd)
 	}
 
 	file(destination: string): RemoteFile {
@@ -123,11 +167,17 @@ function pipe(input: Readable, output: Writable): Promise<unknown> {
 	}
 
 	upload(path: string, options?: UploadOptions): Promise<any> {
-		return undefined
+		const data = createReadStream(path)
+		const { contentType: ContentType, cacheControl: CacheControl } = options?.metadata
+		const Key = options?.destination || path
+		const cmd = new PutObjectCommand({ Bucket: this.bucketName, Key, ContentType, CacheControl, Body: data })
+		return this.s3.send(cmd)
 	}
 
 	async getFiles(options?: GetFilesOptions): Promise<RemoteFile[]> {
-		return undefined
+		const cmd = new ListObjectsCommand({ Bucket: this.bucketName, Prefix:options?.prefix })
+		const response = await this.s3.send(cmd)
+		return response.Contents.map(obj => new S3RemoteFile(this.s3, this.bucketName, obj.Key))
 	}
  }
 
@@ -151,7 +201,7 @@ const provider: StorageProvider = {
 		return original as StorageKey
 	},
 	getClient: (namespace: string, apiHost: string, web: boolean, credentials: Record<string, any>) => {
-		const s3 = new S3(credentials)
+		const s3 = new S3Client(credentials)
 		let bucketName = computeBucketStorageName(apiHost, namespace)
 		let url: string
 		if (web) {
@@ -159,7 +209,7 @@ const provider: StorageProvider = {
 		} else {
 			bucketName = 'data-' + bucketName
 		}
-		return new S3Client(s3, bucketName, url)
+		return new NimS3Client(s3, bucketName, url)
 	}
 }
 
