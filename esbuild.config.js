79511e8f49222031ee1839415e70f6e53e4e6f15
James Thomas <james@jamesthom.as>
2021-06-15 11:24:09 +0100

    Moving TS compilation to ESBuild. (#129)
    This massively speeds up the build & minification by orders of magnitude.
    It makes the release process (which runs the build multiple times) faster.

---
+++ esbuild.config.js
@@ -0,0 +1,42 @@
+const esbuild = require('esbuild')
+const globby = require('globby')
+const rimraf = require('rimraf')
+
+// Automatically exclude all node_modules from the bundled version
+const { nodeExternalsPlugin } = require('esbuild-node-externals')
+
+const exit = () => process.exit(1)
+
+const removeExistingBuildOutput = async () => {
+  return new Promise((resolve, reject) => {
+    rimraf('./lib', (err) => {
+      if (err) return reject(err)
+      resolve()
+    })
+  })
+}
+
+const findSourceFiles = async () => globby('./src/**/*.ts')
+
+const build = async files => {
+  console.log(`starting esbuild with ${files.length} source files.`)
+  console.time('build finished in')
+  return esbuild.build({
+    entryPoints: files,
+    format: 'cjs',
+    outdir: 'lib',
+    // bundle & minify output file
+    bundle: false,
+    minify: true,
+    platform: 'node',
+    sourcemap: true,
+    target: 'node14',
+    plugins: [nodeExternalsPlugin()]
+  })
+}
+
+removeExistingBuildOutput()
+  .then(findSourceFiles)
+  .then(build)
+  .then(() => console.timeEnd('build finished in'))
+  .catch(exit)
