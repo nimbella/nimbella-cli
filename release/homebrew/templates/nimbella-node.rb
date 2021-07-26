505664c99a6693ed7aafa4380ad9ce9c2a7b8e6e
crai <dev.chandan.rai@gmail.com>
2021-07-27 00:19:41 +0530

    brew automation

---
+++ release/homebrew/templates/nimbella-node.rb
@@ -0,0 +1,19 @@
+
+class NimbellaNode < Formula
+  desc "node.js dependency for nimbella"
+  homepage "https://docs.nimbella.com/command-summary"
+  license "Apache-2.0"
+  url "__NODE_BIN_URL__"
+  version "__NODE_VERSION__"
+  sha256 "__NODE_SHA256__"
+  keg_only "nimbella-node is only used by Nimbella CLI (nimbella/brew/nimbella), which explicitly requires from Cellar"
+
+  def install
+    bin.install buildpath/"bin/node"
+  end
+
+  def test
+    output = system bin/"node", "version"
+    assert output.strip == "v#{version}"
+  end
+end
