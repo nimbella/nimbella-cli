04cef73138d712b1b12a18e229abfde7d4ddee28
Joshua Auerbach <joshuaauerbachwatson@gmail.com>
2021-08-03 14:27:06 -0400

    Support --quote on nim auth env

--- src/commands/auth/env.ts
+++ src/commands/auth/env.ts
@@ -13,6 +13,7 @@
 
 import { NimBaseCommand, NimLogger, getCredentials, authPersister } from '@nimbella/nimbella-deployer'
 import { StorageKey } from '@nimbella/storage'
+import { flags } from '@oclif/command'
 
 // Theoretically, there is no guarantee that the following constants align with conventions in @nimbella/storage.
 // In practice, IMO, it is safe to assume that these strings are "well-known" and cannot easily change.
@@ -24,7 +25,12 @@ export default class AuthEnv extends NimBaseCommand {
 
   static args = []
 
-  async runCommand(_rawArgv: string[], _argv: string[], _args: any, _flags: any, logger: NimLogger): Promise<void> {
+  static flags = {
+    quote: flags.boolean({ description: 'Escape and re-quote JSON to survive parsing by a shell' }),
+    ...NimBaseCommand.flags
+  }
+
+  async runCommand(_rawArgv: string[], _argv: string[], _args: any, flags: any, logger: NimLogger): Promise<void> {
     const creds = await getCredentials(authPersister).catch(err => logger.handleError('', err))
     const storage = creds.storageKey as StorageKey
     if (!storage) {
@@ -40,18 +46,26 @@ export default class AuthEnv extends NimBaseCommand {
       // The local storage form for this provider differs from what needs to be in the environment (historical).
       const { credentials, project_id } = storage
       const { client_email, private_key } = credentials
-      printOutput('__NIM_STORAGE_KEY', JSON.stringify({ client_email, private_key, project_id }), logger)
+      printOutput('__NIM_STORAGE_KEY', JSON.stringify({ client_email, private_key, project_id }), logger, flags.quote)
     } else if (provider === S3_PROVIDER) {
       // The S3 provider assumes what's in the environment === what would be stored.
-      printOutput('__NIM_STORAGE_KEY', JSON.stringify(storage), logger)
+      printOutput('__NIM_STORAGE_KEY', JSON.stringify(storage), logger, flags.quote)
     } else {
       logger.handleError(`No support for storage provider '${provider}'`)
     }
-    printOutput('__OW_NAMESPACE', namespace, logger)
-    printOutput('__OW_API_HOST', apiHost, logger)
+    printOutput('__OW_NAMESPACE', namespace, logger, false)
+    printOutput('__OW_API_HOST', apiHost, logger, false)
   }
 }
 
-function printOutput(key: string, value: string, logger: NimLogger) {
+function escapeAndQuote(input: string): string {
+  input = input.replace(/"/g, '\\"')
+  return `"${input}"`
+}
+
+function printOutput(key: string, value: string, logger: NimLogger, quote: boolean) {
+  if (quote) {
+    value = escapeAndQuote(value)
+  }
   logger.log(`${key}=${value}`)
 }
