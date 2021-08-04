20e75c0a7332ae529e23451a3b1cf5c0aa30c350
Joshua Auerbach <joshuaauerbachwatson@gmail.com>
2021-08-04 07:58:09 -0400

    Adjust copyright

--- src/commands/auth/env.ts
+++ src/commands/auth/env.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 - present Nimbella Corp.
+ * Copyright (c) 2021 - present Joshua Auerbach
  *
  * This file is licensed to you under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License. You may obtain a copy
